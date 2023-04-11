9ce6d68eadff57def4617c15d50f441e12be20da
Iain Dillingham <iain@dillingham.me.uk>
2023-04-11 16:58:59 +0100

    Plot aggregated event counts
    We wish to plot sum-by-day on top of mean-by-week. However, the
    aggregates are stored in different data frames with different indexes
    (one at day frequency, one at week frequency). Consequently, we iterate
    over both column-by-column yielding a plot as we go.

--- analysis/utils.py
+++ analysis/utils.py
@@ -1,6 +1,8 @@
 import functools
 import os
 import pathlib
+import re
+import unicodedata
 
 
 WORKSPACE_DIR = pathlib.Path(__file__).parents[1]
@@ -8,3 +10,18 @@ WORKSPACE_DIR = pathlib.Path(__file__).parents[1]
 OUTPUT_DIR = WORKSPACE_DIR / "output"
 
 makedirs = functools.partial(os.makedirs, exist_ok=True)
+
+
+def slugify(s):
+    # Based on Django's slugify. For more information, see:
+    # https://github.com/django/django/blob/4.1.7/django/utils/text.py#L399-L417
+
+    # convert to ASCII
+    s = unicodedata.normalize("NFKD", s).encode("ascii", "ignore").decode("ascii")
+    # remove characters that are not word, white space, or dash
+    s = re.sub(r"[^\w\s-]", "", s)
+    # replace one or more dash or one or more white space with one dash
+    s = re.sub(r"[-\s]+", "-", s)
+    # remove leading and trailing dashes and underscores
+    s = s.strip("-_")
+    return s.lower()
