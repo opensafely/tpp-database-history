533686926230e967fce4e081ec35d72a9ff4310c
Iain Dillingham <iain@dillingham.me.uk>
2023-04-20 14:37:13 +0100

    Test `resample` with "mean"
    `resample` contains a call to `groupby` followed by a call to
    `aggregate`. Different aggregation functions behave differently when
    they are passed empty groups. We capture the differences between "sum"
    (`numpy.sum`) and "mean" (`numpy.mean`) here. The former returns zero;
    the latter returns the missing value marker.

--- tests/test_aggregate.py
+++ tests/test_aggregate.py
@@ -1,7 +1,16 @@
+import functools
+
 import pandas
 import pytest
 
 from analysis import aggregate
+
+
+assert_series_equal = functools.partial(
+    pandas.testing.assert_series_equal,
+    check_dtype=False,
+    check_names=False,
+)
 
 
 def test_read_with_unparsable_date(tmp_path):
@@ -20,7 +29,7 @@ def make_series(event_dates):
     return pandas.Series(index=index, data=1, name="event_count")
 
 
-@pytest.mark.parametrize("func,exp", [("sum", [1, 0, 1])])
+@pytest.mark.parametrize("func,exp", [("sum", [1, 0, 1]), ("mean", [1, None, 1])])
 def test_resample(func, exp):
     series = make_series(["2023-01-01", "2023-01-03"])
     by_day = aggregate.resample(series, "D", func).reset_index()
@@ -29,4 +38,4 @@ def test_resample(func, exp):
         "2023-01-02",
         "2023-01-03",
     ] * 2
-    assert list(by_day["event_count"]) == exp * 2
+    assert_series_equal(by_day["event_count"], pandas.Series(exp * 2))
