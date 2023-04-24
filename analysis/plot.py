93614404393fc35b581b8170824cd2e757523ebe
Iain Dillingham <iain@dillingham.me.uk>
2023-04-24 11:02:11 +0100

    Emphasise week over day
    We emphasise week (mean) counts over day counts by setting the line
    width (in points).

--- analysis/plot.py
+++ analysis/plot.py
@@ -47,8 +47,8 @@ def plot(by_day, by_week):
             ),
         )
 
-        ax.plot(by_day.index, by_day[col], label="Day")
-        ax.plot(by_week.index, by_week[col], label="Week (mean)")
+        ax.plot(by_day.index, by_day[col], linewidth=1, label="Day")
+        ax.plot(by_week.index, by_week[col], linewidth=2, label="Week (mean)")
 
         ax.grid(True)
         ax.set_title(f"From {min_ts:%Y-%m-%d} to {max_ts:%Y-%m-%d}", fontsize="medium")
