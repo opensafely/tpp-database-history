522bcf71fbc237d0a0fdfd396c6ecdc560028951
Iain Dillingham <iain@dillingham.me.uk>
2023-12-12 18:30:23 +0000

    Add `test_aggregate_sum_by_day`
    This complements `test_aggregate_mean_by_week` and replaces the
    low-value `test_resample`.

--- tests/test_aggregate.py
+++ tests/test_aggregate.py
@@ -1,17 +1,8 @@
-import functools
-
 import pandas
 import pytest
 from pandas.testing import assert_frame_equal
 
 from analysis import aggregate
-
-
-assert_series_equal = functools.partial(
-    pandas.testing.assert_series_equal,
-    check_dtype=False,
-    check_names=False,
-)
 
 
 def test_read_with_unparsable_date(tmp_path):
@@ -30,6 +21,23 @@ def make_series(event_dates):
     return pandas.Series(index=index, data=1, name="event_count")
 
 
+def test_aggregate_sum_by_day():
+    series = make_series(["2023-01-01", "2023-01-03"])
+    series.loc[("table_1",)] = 7
+    series.loc[("table_2",)] = 8
+    sum_by_day = aggregate.aggregate(series, "D", "sum")
+    assert_frame_equal(
+        sum_by_day,
+        pandas.DataFrame(
+            [(0, 10), (0, 0), (0, 10)],  # rows
+            index=pandas.DatetimeIndex(
+                ["2023-01-01", "2023-01-02", "2023-01-03"], name="event_date"
+            ),
+            columns=pandas.Index(["table_1", "table_2"], name="table_name"),
+        ),
+    )
+
+
 def test_aggregate_mean_by_week():
     series = make_series(["2023-01-01", "2023-01-03"])
     series.loc[("table_1",)] = 7.1
@@ -43,16 +51,3 @@ def test_aggregate_mean_by_week():
             columns=pandas.Index(["table_1", "table_2"], name="table_name"),
         ),
     )
-
-
-@pytest.mark.parametrize("func,exp", [("sum", [1, 0, 1]), ("mean", [1, 0, 1])])
-def test_resample(func, exp):
-    series = make_series(["2023-01-01", "2023-01-03"])
-    by_day = aggregate.resample(series, "D", func).reset_index()
-    assert_series_equal(
-        by_day["event_date"],
-        pandas.Series(
-            pandas.to_datetime(["2023-01-01", "2023-01-02", "2023-01-03"] * 2)
-        ),
-    )
-    assert_series_equal(by_day["event_count"], pandas.Series(exp * 2))
