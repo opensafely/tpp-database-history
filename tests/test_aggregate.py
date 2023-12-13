86f11e3ba5dce3171b3605f085f27ae92eede7b0
Iain Dillingham <iain@dillingham.me.uk>
2023-12-13 11:44:17 +0000

    Group test functions

--- tests/test_aggregate.py
+++ tests/test_aggregate.py
@@ -5,20 +5,20 @@ from pandas.testing import assert_frame_equal
 from analysis import aggregate
 
 
-def test_read_with_unparsable_date(tmp_path):
-    rows_csv = tmp_path / "rows.csv"
-    unparsable_date = "9999-01-01"
-    rows_csv.write_text(f"table_name,event_date,event_count\nAPCS,{unparsable_date},1")
-    with pytest.raises(AssertionError):
-        aggregate.read(rows_csv)
-
-
 def make_series(event_dates):
     index = pandas.MultiIndex.from_product(
         (["table_1", "table_2"], pandas.to_datetime(event_dates)),
         names=("table_name", "event_date"),
     )
     return pandas.Series(index=index, data=1, name="event_count")
+
+
+def test_read_with_unparsable_date(tmp_path):
+    rows_csv = tmp_path / "rows.csv"
+    unparsable_date = "9999-01-01"
+    rows_csv.write_text(f"table_name,event_date,event_count\nAPCS,{unparsable_date},1")
+    with pytest.raises(AssertionError):
+        aggregate.read(rows_csv)
 
 
 def test_aggregate_sum_by_day():
