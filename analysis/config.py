1e7d6fe0c269a8be82606710b51f2bd3af19d0b1
Iain Dillingham <iain@dillingham.me.uk>
2023-12-15 10:47:29 +0000

    Fix the report run date
    Closes #45

--- analysis/config.py
+++
@@ -1,8 +0,0 @@
-import datetime
-
-
-# The value of RUN_DATE should be the date analysis/query.sql was run. However, this
-# date is not knowable, so instead we use today's date, which is the same as @to_date in
-# analysis/query.sql. For more information, see:
-# https://github.com/opensafely/tpp-database-history/issues/45
-RUN_DATE = datetime.date.today()
