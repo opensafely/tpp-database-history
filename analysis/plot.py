2f861e88b868baab4ea4691839aa786e00f5fab8
Iain Dillingham <iain@dillingham.me.uk>
2023-12-18 17:30:47 +0000

    `col` -> `table_name`

--- analysis/plot.py
+++ analysis/plot.py
@@ -67,11 +67,11 @@ def main(from_date, from_offset, d_out):
     utils.makedirs(d_out)
 
     metadata = {"paths": {}}
-    figs_cols = plot(by_day, by_week, get_plot_title(from_date, from_offset))
-    for fig, col in figs_cols:
-        f_stem = slugify(col)
+    figs_table_names = plot(by_day, by_week, get_plot_title(from_date, from_offset))
+    for fig, table_name in figs_table_names:
+        f_stem = slugify(table_name)
         f_path = d_out / f"{f_stem}.png"
-        metadata["paths"][col] = str(f_path)
+        metadata["paths"][table_name] = str(f_path)
         fig.savefig(f_path)
 
     with (d_out / "metadata.json").open("w") as fp:
